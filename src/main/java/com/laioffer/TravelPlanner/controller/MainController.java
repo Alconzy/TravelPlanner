f2dd104589ce95fba4995c1eb5cf9aba5f0651e8
alconzy <alangb013@gmail.com>
2020-12-01 00:47:10 -0500

    login feature

--- src/main/java/com/laioffer/TravelPlanner/controller/MainController.java
+++ src/main/java/com/laioffer/TravelPlanner/controller/MainController.java
@@ -1,17 +1,19 @@
 package com.laioffer.TravelPlanner.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.laioffer.TravelPlanner.entity.User;
 import com.laioffer.TravelPlanner.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class MainController {
-
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
     @Autowired
     private UserRepository userRepository;
 
@@ -24,7 +26,26 @@ public class MainController {
         return "Saved";
     }
 
-
+    @GetMapping(path = "/login")
+    public String login(@RequestParam String email, @RequestParam String password, Model model) {
+        List<User> users = userRepository.findByEmail(email);
+        if (users == null) {
+            log.warn("attempting to log in with the non-existed account");
+            return "User has not registered";
+        } else {
+            User user = users.get(0);
+            if (user.getPassword().equals(password)) {
+                //todo, return name value to front, assume related el expression is ${name}
+                model.addAttribute("name", user.getFirstName()+ " " + user.getLastName());
+                log.warn(user.toString()+ " logged in");
+            } else {
+                model.addAttribute("name", "logging failed, password incorrect");
+                log.warn(user.toString()+ " failed to log in");
+            }
+            //todo, assume login page is index.html
+            return "index";
+        }
+    }
 
 
 
