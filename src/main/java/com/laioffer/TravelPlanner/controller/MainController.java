e4635cd38d4e8f47bf006462a35c19706b856e11
ericpanyc <yachenpan1005@gmail.com>
2020-12-05 23:39:09 -0800

    register respond json

--- src/main/java/com/laioffer/TravelPlanner/controller/MainController.java
+++ src/main/java/com/laioffer/TravelPlanner/controller/MainController.java
@@ -3,6 +3,7 @@ package com.laioffer.TravelPlanner.controller;
 import javax.validation.Valid;
 
 import com.laioffer.TravelPlanner.entity.Itinerary;
+import com.laioffer.TravelPlanner.entity.RegisterResponseBody;
 import com.laioffer.TravelPlanner.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +11,6 @@ import com.laioffer.TravelPlanner.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,34 +18,30 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Date;
 import java.util.List;
 
-
-@Controller
+@RestController
 public class MainController {
     private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
     @Autowired
     private UserService userService;
 
+    @PostMapping(value = "/register")
+    public RegisterResponseBody register(@RequestBody User user) {
+        RegisterResponseBody registerResponseBody;
 
-    @RequestMapping(value= {"/signup"}, method=RequestMethod.POST)
-    public ModelAndView createUser(@Valid User user, BindingResult bindingResult) {
-        ModelAndView model = new ModelAndView();
         User userExists = userService.findUserByEmail(user.getEmail());
 
         if(userExists != null) {
-            bindingResult.rejectValue("email", "error.user", "This email already exists!");
-        }
-        if(bindingResult.hasErrors()) {
-            model.setViewName("user/signup");
+            registerResponseBody = new RegisterResponseBody("failed",userExists.getEmail(),"already exists");
         } else {
             userService.saveUser(user);
-            model.addObject("msg", "User has been registered successfully!");
-            model.addObject("user", new User());
-            model.setViewName("user/signup");
+            registerResponseBody = new RegisterResponseBody("success",user.getEmail(),"registered");
         }
-
-        return model;
+        return registerResponseBody;
     }
+
+
+
 
     @RequestMapping(value= {"/"}, method=RequestMethod.GET)
     public ModelAndView home() {
@@ -54,7 +50,7 @@ public class MainController {
 //        User user = userService.findUserByEmail(auth.getName());
 //
 //        model.addObject("userName", user.getFirstName() + " " + user.getLastName());
-        model.setViewName("home/home");
+        model.setViewName("home");
         return model;
     }
 
