a77b9ed2364a1a02e2e1d0082baadf00d83ac427
Eric <54253195+ericpanyc@users.noreply.github.com>
2020-12-07 15:32:29 -0800

    minor change (#11)

--- src/main/java/com/laioffer/TravelPlanner/controller/MainController.java
+++ src/main/java/com/laioffer/TravelPlanner/controller/MainController.java
@@ -13,9 +13,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -63,8 +60,6 @@ public class MainController {
         Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
         String userName = loggedInUser.getName();
         User user = userService.findUserByEmail(userName);
-
-
         List<GetHistoryResponseBody> response = itineraryService.getHistory(user,request.startDate, request.endDate);
         return response;
     }
@@ -85,40 +80,8 @@ public class MainController {
     @RequestMapping(value= {"/"}, method=RequestMethod.GET)
     public ModelAndView home() {
         ModelAndView model = new ModelAndView();
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        User user = userService.findUserByEmail(auth.getName());
-//
-//        model.addObject("userName", user.getFirstName() + " " + user.getLastName());
         model.setViewName("home");
         return model;
     }
-
-
-
-//    @RequestMapping(value= {"/access_denied"}, method=RequestMethod.GET)
-//    public ModelAndView accessDenied() {
-//        ModelAndView model = new ModelAndView();
-//        model.setViewName("errors/access_denied");
-//        return model;
-//    }
-
-    @RequestMapping(value= {"/history/{startDate}/{endDate}"},method = RequestMethod.GET)
-    public ModelAndView history(@PathVariable(value = "startDate") Date startDate, @PathVariable(value = "endDate") Date endDate){
-        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
-        String userName = loggedInUser.getName();
-        User user = userService.findUserByEmail(userName);
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("user/history");
-        List<Itinerary> itinerary = user.getItinerary();
-        modelAndView.addObject("msg", "User Itinerary History");
-
-//        for(int i = 0; i<itinerary.size(); i++){
-//            if(itinerary.get(i).getStartDate().compareTo(startDate) >0  &&  itinerary.get(i).getEndDate().compareTo(endDate) <0){
-//                modelAndView.addObject("itinerary",itinerary.get(i));
-//            }
-//        }
-        return modelAndView;
-    }
-
 }
 
